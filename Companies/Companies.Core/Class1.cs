6676a7442bbec4740adc09a68244503e3e0094e3
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2018-08-25 11:53:25 +0200

    added .Core project

---
+++ Companies/Companies.Core/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Companies.Core
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class Department {
+        public int Id { get; set; }
+    }
+
+    public class Employee {
+        public int Id { get; set;}
+    }
+}
