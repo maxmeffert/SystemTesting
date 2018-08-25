30cfa8a9cdff967fdcf667491c6bc11035fe965b
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2018-08-25 12:01:32 +0200

    started systemtesting infrastructure

--- Companies/Companies.Tests/UnitTest1.cs
+++ Companies/Companies.Tests/UnitTest1.cs
@@ -1,18 +1,36 @@
 using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace Tests
 {
     public class Tests
     {
+        [OneTimeSetUp]
+        public static async Task OneTimeSetup()
+        {
+            var t = new Thread(() => {
+                Companies.Api.Program.Main(new string[0]);
+            });
+            t.Start();
+            await Task.Delay(5000);
+        }
+
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public async Task Test1()
         {
-            Assert.Pass();
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("http://localhost:5000/api/values");
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+            Assert.AreEqual("[\"value1\",\"value2\"]", content);
         }
     }
 }
