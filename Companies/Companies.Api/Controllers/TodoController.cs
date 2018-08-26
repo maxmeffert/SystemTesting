76ab87905b0bdfbc9a6fd7c6f66e606151214de2
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2018-08-26 13:28:17 +0200

    added a controller

--- Companies/Companies.Api/Controllers/TodoController.cs
+++ Companies/Companies.Api/Controllers/TodoController.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Companies.Api.Controllers {
 
@@ -16,6 +19,27 @@ namespace Companies.Api.Controllers {
         }
 
         public DbSet<TodoItem> TodoItems {get; set;}
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase {
+
+        private readonly TodoContext _todoContext;
+
+        public TodoController(TodoContext todoContext) {
+            _todoContext = todoContext;
+
+            // _todoContext.TodoItems.Add(new TodoItem {
+            //     Name = "asdf"
+            // });
+            // _todoContext.SaveChanges();
+        }
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll() {
+            return _todoContext.TodoItems.ToList();
+        }
     }
 
 }
