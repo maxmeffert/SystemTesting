c08ba220b9fb0b7ec14120b0a6a445779cfdb2c3
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2018-08-26 13:40:04 +0200

    added Yata aka 'Yet another todo app' as systemtesting solution because a todo app is smaller than a companies example app would be

---
+++ Yata/Yata.Client/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Yata.Client
+{
+    public class Class1
+    {
+    }
+}
---
+++ Yata/Yata.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Yata.Core
+{
+    public class Class1
+    {
+    }
+}
