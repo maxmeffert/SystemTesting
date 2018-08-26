8900a2ea21688beab1850e419fb88c8afb1505ba
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2018-08-26 13:48:22 +0200

    the simple todo model

---
+++ Yata/Yata.Core/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Yata.Core
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
