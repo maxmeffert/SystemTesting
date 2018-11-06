bd63dd5afe60c759012071df349b029f22bf5bcb
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2018-11-06 23:56:58 +0100

    added formatted test case names

--- Companies/Companies.SystemTests/SystemTestCases.cs
+++ Companies/Companies.SystemTests/SystemTestCases.cs
@@ -1,12 +1,33 @@
-using System;
-using System.Collections.Generic;
 using Companies.SystemTests.Scenarios;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Companies.SystemTests
 {
     public static class SystemTestCases
     {
-        public static IEnumerable<BaseSystemTestCase> All()
+        public static IEnumerable All()
+        {
+            return AllSystemTestCases().Select(CreateTestCaseData);
+        }
+
+        private static TestCaseData CreateTestCaseData(ISystemTestCase systemTestCase)
+        {
+            return new TestCaseData(systemTestCase)
+            {
+                TestName = CreateTestName(systemTestCase)
+            };
+        }
+
+        private static string CreateTestName(ISystemTestCase systemTestCase)
+        {
+            return $"Scenario: {systemTestCase.GetType().Name}";
+        }
+
+        private static IEnumerable<ISystemTestCase> AllSystemTestCases()
         {
             yield return new DoNothing();
             yield return new Scenario1();
