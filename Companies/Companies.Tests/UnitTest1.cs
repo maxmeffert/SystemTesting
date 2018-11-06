7790ce506a93829e13f5a2fb2856a92a0c58c6cd
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2018-11-06 22:57:26 +0100

    cleanup

--- Companies/Companies.Tests/UnitTest1.cs
+++ Companies/Companies.Tests/UnitTest1.cs
@@ -4,19 +4,11 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Net.Http;
 
-namespace Tests
+namespace Companies.Tests
 {
     public class Tests
     {
-        [OneTimeSetUp]
-        public static async Task OneTimeSetup()
-        {
-            var t = new Thread(() => {
-                Companies.Api.Program.Main(new string[0]);
-            });
-            t.Start();
-            await Task.Delay(5000);
-        }
+        
 
         [SetUp]
         public void Setup()
@@ -26,11 +18,7 @@ namespace Tests
         [Test]
         public async Task Test1()
         {
-            var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync("http://localhost:5000/api/values");
-            var content = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(content);
-            Assert.AreEqual("[\"value1\",\"value2\"]", content);
+            
         }
     }
 }
